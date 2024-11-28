b1a95405e05dc0ca0ba21762d7d9f80ec3301a02
YashwanthYM <153629062+YashwanthYM@users.noreply.github.com>
2024-11-29 05:18:31 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,53 +1,39 @@
-document.addEventListener('DOMContentLoaded', function() {
-  // Search-box open/close
-  let navbar = document.querySelector(".navbar");
-  let searchBox = document.querySelector(".search-box .bx-search");
+// search-box open close js code
+let navbar = document.querySelector(".navbar");
+let searchBox = document.querySelector(".search-box .bx-search");
+// let searchBoxCancel = document.querySelector(".search-box .bx-x");
 
-  if (searchBox && navbar) {
-    searchBox.addEventListener("click", () => {
-      navbar.classList.toggle("showInput");
-      if (navbar.classList.contains("showInput")) {
-        searchBox.classList.replace("bx-search", "bx-x");
-      } else {
-        searchBox.classList.replace("bx-x", "bx-search");
-      }
-    });
-  }
-
-  // Sidebar open/close
-  let navLinks = document.querySelector(".nav-links");
-  let menuOpenBtn = document.querySelector(".navbar .bx-menu");
-  let menuCloseBtn = document.querySelector(".nav-links .bx-x");
-
-  if (menuOpenBtn && menuCloseBtn && navLinks) {
-    menuOpenBtn.onclick = function() {
-      navLinks.style.left = "0";
-    };
-    menuCloseBtn.onclick = function() {
-      navLinks.style.left = "-100%";
-    };
-  }
-
-  // Sidebar submenu open/close
-  let htmlcssArrow = document.querySelector(".htmlcss-arrow");
-  let moreArrow = document.querySelector(".more-arrow");
-  let jsArrow = document.querySelector(".js-arrow");
-
-  if (htmlcssArrow) {
-    htmlcssArrow.onclick = function() {
-      navLinks.classList.toggle("show1");
-    };
-  }
-
-  if (moreArrow) {
-    moreArrow.onclick = function() {
-      navLinks.classList.toggle("show2");
-    };
-  }
-
-  if (jsArrow) {
-    jsArrow.onclick = function() {
-      navLinks.classList.toggle("show3");
-    };
+searchBox.addEventListener("click", ()=>{
+  navbar.classList.toggle("showInput");
+  if(navbar.classList.contains("showInput")){
+    searchBox.classList.replace("bx-search" ,"bx-x");
+  }else {
+    searchBox.classList.replace("bx-x" ,"bx-search");
   }
 });
+
+// sidebar open close js code
+let navLinks = document.querySelector(".nav-links");
+let menuOpenBtn = document.querySelector(".navbar .bx-menu");
+let menuCloseBtn = document.querySelector(".nav-links .bx-x");
+menuOpenBtn.onclick = function() {
+navLinks.style.left = "0";
+}
+menuCloseBtn.onclick = function() {
+navLinks.style.left = "-100%";
+}
+
+
+// sidebar submenu open close js code
+let htmlcssArrow = document.querySelector(".htmlcss-arrow");
+htmlcssArrow.onclick = function() {
+ navLinks.classList.toggle("show1");
+}
+let moreArrow = document.querySelector(".more-arrow");
+moreArrow.onclick = function() {
+ navLinks.classList.toggle("show2");
+}
+let jsArrow = document.querySelector(".js-arrow");
+jsArrow.onclick = function() {
+ navLinks.classList.toggle("show3");
+}
